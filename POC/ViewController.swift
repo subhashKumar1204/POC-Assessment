c31baa788a8bec4edfccb526ad2d23723c1f819f
subhash kumar <subhashk@moblize.com>
2020-08-27 12:50:16 +0530

    Initial Commit

---
+++ POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC
+//
+//  Created by SUBHASH KUMAR on 27/08/20.
+//  Copyright © 2020 Subhash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ POCTests/POCTests.swift
@@ -0,0 +1,34 @@
+//
+//  POCTests.swift
+//  POCTests
+//
+//  Created by SUBHASH KUMAR on 27/08/20.
+//  Copyright © 2020 Subhash. All rights reserved.
+//
+
+import XCTest
+@testable import POC
+
+class POCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
